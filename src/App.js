e356cd5d5a7e019876d44d319198f6c25e96d53d
Ubongg <ubongeffiong5@gmail.com>
2022-02-18 13:25:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleProduct from "./pages/SingleProduct";
+import ProductList from "./pages/ProductList";
+import CartContainer from "./pages/CartContainer";
+import Error from "./pages/Error";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartContainer />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
